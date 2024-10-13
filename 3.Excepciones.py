741d0b8ba4be7ce848e9d2b09c9aa60d29ad86f3
Macarena Caro <m.caro.cortes2@gmail.com>
2024-10-13 22:33:00 +0200

    excepciones

--- 3.Excepciones.py
+++ 3.Excepciones.py
@@ -3,23 +3,23 @@ números impares desde 1 hasta ese número, separados por comas. Si el número i
 o no es positivo, se lo deberá volver a pedir las veces que sean necesarias antes de hacer el conteo,
 mostrando el mensaje de error correspondiente (por ejemplo, Número no válido)."""
    
-
-
 correcto = False
 while not correcto:
     try:
-        n1 = int(input("Ingresa: "))
-
+        n1 = int(input("Ingresa numero positivo: "))
 
         if n1 <= 0:
             raise Exception()
        
-        list = ""
+        list=""
         for i in range(n1):
          if i % 2 != 0:
-            list = str(i)
+             list += str(i) + ","
         correcto = True
-
 
     except:
          print("Advertencia: el valor ingresado no es un número válido. Intenta de nuevo...")
+
+# Eliminar la última coma sobrante
+list = list.rstrip(",")
+print(list)
