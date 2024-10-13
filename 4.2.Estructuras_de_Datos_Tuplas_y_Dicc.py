b005372a97a975de0b3668a2b1fc3b02b778d4d4
Macarena Caro <m.caro.cortes2@gmail.com>
2024-10-13 22:57:33 +0200

    cambios

---
+++ 4.2.Estructuras_de_Datos_Tuplas_y_Dicc.py
@@ -0,0 +1,14 @@
+"""Ejercicio 1
+
+Crea un programa llamado Tuplas.py donde le pidas al usuario que rellene su dirección postal, 
+que estará formada por su nombre de calle (texto), número de puerta (entero) y número de piso (entero). 
+Almacena los datos en una tupla y luego muestra por pantalla el resultado (campo a campo)."""
+
+
+"""Ejercicio 2
+
+Crea un programa llamado DiccionarioTuplas.py donde le pidas al usuario que rellene direcciones de 4 usuarios, 
+identificados por su clave que será el DNI. Así, para cada usuario rellenará dicho DNI, y luego los datos de 
+la dirección como en el ejercicio anterior (nombre de calle, número de puerta y número de piso). Almacenará los
+ datos en un diccionario (asociando cada DNI con su dirección) y luego le pedirá al usuario que escriba un DNI 
+ y mostrará los datos de su dirección, o el mensaje "El DNI no se encuentra almacenado" si no existe dicha clave."""
